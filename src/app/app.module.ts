2881ec99a025cdd61fd0ce7c9f374e6863f5bc08
giftkari <giftkari@yahoo.com>
2020-11-14 14:32:40 +1000

    First Preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ClarityModule } from '@clr/angular';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -11,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    ClarityModule,
+   
     FormsModule,
     BrowserAnimationsModule
   ],
