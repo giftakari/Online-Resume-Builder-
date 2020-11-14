27365341aba9ed13c878987d1e1fe7bbc2818948
giftkari <giftkari@yahoo.com>
2020-11-14 12:46:52 +1000

    upload component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'online-resume-builder';
+
+  generatePdf() {
+    const documentDefination = { content: 'This is the sample of the printed pdf' };
+    pdfMake.createPdf(documentDefination).open();
+  }
+
+
 }
